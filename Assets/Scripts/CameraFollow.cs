b66878b34c74b5dd47760785194ea4900bb86648
Bolza <stefano.sergio@gmail.com>
2016-11-05 16:35:16 +0000

    camera linear interpolation

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	public Camera cam;
+	public int pixelPerUnit = 32;
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		cam.orthographicSize = Screen.height / pixelPerUnit / 4;
+		if (target) {
+			Vector3 z = new Vector3 (0, 0, -1);
+			transform.position = Vector3.Lerp (transform.position, target.position, 0.1f) + z;
+		}
+	}
+}
